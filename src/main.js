27ec09b942f0d18f54f5a34703388a218a9102da
chuck <121477964@qq.com>
2018-12-16 18:01:46 +0800

    将less和css的栗子写在了首页

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import './style.css';
 
 const component = ()=> {
-  var element = document.getElementById('app');
+  const element = document.getElementById('app');
   const str = 'webpackDemo';
   element.innerHTML = str;
   return element;
 }
 
+const less = () => {
+  const element = document.createElement('div');
+  element.setAttribute('id', 'less');
+  const str = 'less编译成功的话背景会变成红色！';
+  element.innerHTML = str;
+  return element;
+};
+
+const css = () => {
+  const element = document.createElement('div');
+  element.setAttribute('id', 'css');
+  const str = 'css编译成功的话背景会变成蓝色！';
+  element.innerHTML = str;
+  return element;
+};
+
 document.body.appendChild(component());
+document.body.appendChild(less());
+document.body.appendChild(css());
